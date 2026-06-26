b1cf626fd6d9e330f6419bd5280a5fb6059b6e89
Dmytro Feshchenko <dmytro.feshchenko@gmail.com>
2026-06-26 17:39:18 +0300

    SF7: fix internal dependency

--- src/DependencyInjection/DmytrofDoctrineModificationEventsExtension.php
+++ src/DependencyInjection/DmytrofDoctrineModificationEventsExtension.php
@@ -12,9 +12,9 @@
 namespace Dmytrof\DoctrineModificationEventsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class DmytrofDoctrineModificationEventsExtension extends Extension
 {
